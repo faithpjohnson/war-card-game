d403ba13f438e1af9f0d4a5fcad1e60285b85d55
Faith Johnson <faithpjohnson@gmail.com>
2021-12-13 13:17:17 -0700

    Add constants, state variables, cached elements, event listeners, outline functions

--- js/main.js
+++ js/main.js
@@ -1,1 +1,51 @@
-console.log('hello');
+/**** Constants ****/
+const cardRank = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'];
+const cardSuit = ['♣', '♦', '♥', '♠'];
+
+
+
+/**** State Variables ****/
+let player1Deck ;
+let player2Deck ;
+let player1Count;
+let player2Count ;
+let war;
+let shuffle;
+let winner;
+
+/**** Cached Elements ****/
+// card count element
+const countEl = {
+    player1: document.querySelector("#player-1-card-count"),
+    player2: document.querySelector("#player-2-card-count")
+};
+const wnrMsgEl = document.querySelector(".winner-msg");
+
+/**** Event Listeners ****/
+// elements to listen for- buttons have been tested
+// draw cards button
+// restart button
+document.querySelector(".drw-btn").addEventListener("click", playRound);
+document.querySelector(".restart-btn").addEventListener("click", restartGame);
+
+
+// initialize start variables 
+function init(){
+
+}
+
+
+function render(){
+
+}
+
+
+
+function playRound(){
+    console.log("PR function is being invoked!")
+}
+
+
+function restartGame(){
+    console.log("RG function is being invoked!")
+}
