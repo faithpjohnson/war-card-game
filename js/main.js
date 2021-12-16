b840150b5956fd155436d895c08b7b8aa5517049
Faith Johnson <faithpjohnson@gmail.com>
2021-12-16 10:27:26 -0700

    Add war conditional

--- js/main.js
+++ js/main.js
@@ -5,13 +5,15 @@ const suits = ['s', 'c', 'd', 'h'];
 /*----- app's state (variables) -----*/
 let player1Cards;
 let player2Cards;
-let war = [];
+let warStack = [];
 let flippedCard1;
 let flippedCard2;
 let gameMessage= "";
+let player1CardValue;
+let player2CardValue;
 
 /*----- cached element references -----*/
-// card count element
+
 let countEl = {
     player1: document.querySelector("#player-1-card-count"),
     player2: document.querySelector("#player-2-card-count")
@@ -25,9 +27,7 @@ const activeCardEl = {
 let winnerMsgEl = document.querySelector(".winner-msg");
 
 /*----- event listeners -----*/
-// elements to listen for- buttons have been tested
-// draw cards button
-// restart button
+
 document.querySelector(".drw-btn").addEventListener("click", drawCards);
 document.querySelector(".restart-btn").addEventListener("click", restartGame);
 
@@ -42,16 +42,11 @@ class Card {
         let classString = `${this.suit}${this.rank}`;
         return classString;
     }
-    // need to fix this- have to assign values to J,K,Q,A
-    // use index of ranks array to assign values?
+
     getCardValue(){
-        // for(let i = 0; i < ranks.length; i++){
-            //  console.log("ranksCollection",ranks[i]);
-            //  console.log("this",this.rank);
         let cardValue = ranks.indexOf(this.rank);
         console.log(`The player has a ${this.rank} and it has value ${cardValue}`);
         return cardValue;
-    
     }
 }
 
@@ -75,7 +70,8 @@ class Deck {
 
     shuffleDeck() {
         let shuffledDeck = [];
-        for (let i = 0; i < 52; i++) {
+        const cardLength = this.cards.length;
+        for (let i = 0; i < cardLength; i++) {
             let rndIdx =  Math.floor(Math.random() * this.cards.length);
             shuffledDeck.push(this.cards.splice(rndIdx, 1)[0]);
         }
@@ -85,14 +81,13 @@ class Deck {
 
     splitPlayerDecks(){
         let player1Cards = this.cards.slice(0, 26);
-        //console.log("test", player1Cards)
+        console.log("test", player1Cards)
 
         let player2Cards = this.cards.slice(26, 52);
-        //console.log("test2", player2Cards)
+        console.log("test2", player2Cards)
         return [player1Cards, player2Cards];
     }
 }
-
 
 init();
 
@@ -114,9 +109,6 @@ function init() {
     drawCards();
 }
 
-// render card count
-// render cards
-// render winner message
 function render() {
     countEl.player1.innerText = player1Cards.length;
     countEl.player2.innerText = player2Cards.length;
@@ -129,8 +121,20 @@ function render() {
     let flippedCard2Class = flippedCard2.getClass();
     activeCardEl.player2Active.classList.add(flippedCard2Class);
 
-    // render winner message when a player reaches 0 cards
     winnerMsgEl.innerText = gameMessage;
+
+    console.log("=====================START==========================");
+    console.log("player1Cards", player1Cards);
+    console.log("player2Cards", player2Cards);
+    console.log("warStack", warStack);
+    console.log("flippedCard1", flippedCard1);
+    console.log("flippedCard2", flippedCard2);
+    console.log("gameMessage", gameMessage);
+    console.log("player1CardValue", player1CardValue);
+    console.log("player2CardValue", player2CardValue);
+    console.log(`cards add up to 52: ${player1Cards.length + player2Cards.length === 52}`);
+    console.log(`cards add up to 52: ${player1Cards.length + player2Cards.length}`);
+    console.log("======================END=========================");
 }
 
 
@@ -139,24 +143,38 @@ function drawCards(){
     flippedCard1 = player1Cards.shift();
     flippedCard2 = player2Cards.shift();
    
-
-    let player1CardValue = flippedCard1.getCardValue();
-    let player2CardValue = flippedCard2.getCardValue();
+    player1CardValue = flippedCard1.getCardValue();
+    player2CardValue = flippedCard2.getCardValue();
     console.log(player1CardValue, player2CardValue);
     
+ 
+    getRoundWinner();
+    render();
+    
+}
+
+function getRoundWinner(){
     if(player1CardValue > player2CardValue){
-        console.log("player1 winssss");
-        //if player1 wins
-        //add player2 card to player1 deck .push
+        console.log("Player 1 Wins!");
         player1Cards.push(flippedCard2);
         player1Cards.push(flippedCard1);
+        player1Cards.push(...warStack);
+        gameMessage = "Player 1 Wins this round";
     } else if(player2CardValue > player1CardValue){
-        console.log("player 2 winssss");
+        console.log("Player 2 Wins!");
         player2Cards.push(flippedCard1);
         player2Cards.push(flippedCard2);
+        player2Cards.push(...warStack);
+        gameMessage = "Player 2 Wins this round";
     } else {
         console.log("WAR");
-        activateWar();
+        gameMessage = "It's WAR!";
+        // put cards in war stack
+        warStack.push(flippedCard1);
+        warStack.push(flippedCard2);
+
+        // draw additional cards from each card stack
+        drawCards();
     }
 
     if(player1Cards.length === 0){
@@ -164,22 +182,6 @@ function drawCards(){
     } else if (player2Cards.length === 0){
         gameMessage= "Player 1 WINS!";
     }
-
-    render();
-}
-
-// 
-function getRoundWinner(){
-
-}
-
-
-function renderWnrMsg(){
-
-}
-
-function activateWar(){
-    console.log("it's war!!");
 }
 
 function restartGame() {
