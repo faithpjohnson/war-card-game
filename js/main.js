c84365f33769bbb8d8c2560b7f69b79a005c9372
Faith Johnson <faithpjohnson@gmail.com>
2021-12-16 23:45:31 -0700

    Clean up

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 /*----- constants -----*/
 const ranks = ['02', '03', '04', '05', '06', '07', '08', '09', '10', 'J', 'Q', 'K', 'A'];
 const suits = ['s', 'c', 'd', 'h'];
-
 /*----- app's state (variables) -----*/
 let player1Cards;
 let player2Cards;
@@ -11,7 +10,7 @@ let flippedCard2;
 let gameMessage = "";
 let player1CardValue;
 let player2CardValue;
-
+let drawButton;
 /*----- cached element references -----*/
 let countEl = {
 	player1: document.querySelector("#player-1-card-count"),
@@ -22,29 +21,27 @@ const activeCardEl = {
 	player2Active: document.querySelector("#player2-active")
 };
 let winnerMsgEl = document.querySelector(".winner-msg");
-
+const drawCardsbutton = document.querySelector(".draw-btn");
 /*----- event listeners -----*/
 document.querySelector(".draw-btn").addEventListener("click", drawCards);
 document.querySelector(".restart-btn").addEventListener("click", restartGame);
-
 /*----- classes -----*/
-// card class to make it easier to interact with card
 class Card {
 	constructor(rank, suit) {
 		this.rank = rank;
 		this.suit = suit;
 	}
-	// helpers
 	getClass() {
+		if (this.rank === "empty") {
+			return "outline";
+		}
 		return `${this.suit}${this.rank}`;
 	}
 	getCardValue() {
 		return ranks.indexOf(this.rank);
 	}
 }
-// deck class to make and interact with a collection of cards
 class Deck {
-	// loop through cards and ranks and create a new array of cards
 	constructor() {
 		let newCards = [];
 		ranks.forEach(function(rank) {
@@ -52,10 +49,8 @@ class Deck {
 				newCards.push(new Card(rank, suit));
 			})
 		})
-		// saving new cards to the cards property on the deck object 
 		this.cards = newCards;
 	}
-	// returns cards property on the deck object
 	getCards() {
 		return this.cards;
 	}
@@ -66,7 +61,6 @@ class Deck {
 			let rndIdx = Math.floor(Math.random() * this.cards.length);
 			shuffledDeck.push(this.cards.splice(rndIdx, 1)[0]);
 		}
-		// save shuffled cards to cards property on the deck object
 		this.cards = shuffledDeck;
 	}
 	splitPlayerDecks() {
@@ -75,44 +69,31 @@ class Deck {
 		return [player1Cards, player2Cards];
 	}
 }
-
 /*----- functions -----*/
 init();
+render();
 
 function init() {
-	// create new instance of the deck obj
 	let tempDeck = new Deck();
-	// shuffle deck
 	tempDeck.shuffleDeck();
-	// split deck between players
 	let playersCards = tempDeck.splitPlayerDecks();
 	player1Cards = playersCards[0];
 	player2Cards = playersCards[1];
-	// activeCardEl.player1Active.className = "card outline xlarge";
-	// let flippedCard1Class = flippedCard1.getClass();
-	// activeCardEl.player1Active.classList.add(flippedCard1Class);
-	// activeCardEl.player2Active.className = "card outline xlarge";
-	// let flippedCard2Class = flippedCard2.getClass();
-	// activeCardEl.player2Active.classList.add(flippedCard2Class);
-	// console.log(flippedCard2Class);
+	gameMessage = "";
+	drawButton = "start game";
+	flippedCard1 = new Card("empty", "empty");
+	flippedCard2 = new Card("empty", "empty");
 }
 
 function render() {
 	countEl.player1.innerHTML = `Card Count: ${player1Cards.length}`;
 	countEl.player2.innerHTML = `Card Count: ${player2Cards.length}`;
-	// activeCardEl.className = "card shadow xlarge";
 	activeCardEl.player1Active.className = "card shadow xlarge";
-	let flippedCard1Class = flippedCard1.getClass();
-	activeCardEl.player1Active.classList.add(flippedCard1Class);
+	activeCardEl.player1Active.classList.add(flippedCard1.getClass());
 	activeCardEl.player2Active.className = "card shadow xlarge";
-	let flippedCard2Class = flippedCard2.getClass();
-	activeCardEl.player2Active.classList.add(flippedCard2Class);
+	activeCardEl.player2Active.classList.add(flippedCard2.getClass());
 	winnerMsgEl.innerText = gameMessage;
-	
-	console.log("warStack", warStack);
-	console.log(`cards add up to 52: ${player1Cards.length + player2Cards.length === 52}`);
-	console.log(`cards add up to 52: ${player1Cards.length + player2Cards.length}`);
-	
+	drawCardsbutton.innerText = drawButton;
 }
 
 function drawCards() {
@@ -129,14 +110,16 @@ function getRoundWinner() {
 		player1Cards.push(flippedCard2, flippedCard1, ...warStack);
 		warStack = [];
 		gameMessage = "Player 1 Wins this round!";
+		drawButton = "draw cards";
 	} else if (player2CardValue > player1CardValue) {
 		player2Cards.push(flippedCard1, flippedCard2, ...warStack);
 		warStack = [];
 		gameMessage = "Player 2 Wins this round!";
+		drawButton = "draw cards";
 	} else {
 		gameMessage = "It's WAR!";
+		drawButton = "draw again for war";
 		warStack.push(flippedCard1, flippedCard2);
-		drawCards();
 	}
 	if (player1Cards.length === 0) {
 		gameMessage = "Player 2 WINS!";
